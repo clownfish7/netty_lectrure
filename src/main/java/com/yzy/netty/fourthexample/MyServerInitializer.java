d4447e67f1a0237878317c88889c156915cdb0d5
clownfish <279505647@qq.com>
2019-06-23 01:27:04 +0800

    netty socker demo server-IdleState

---
+++ src/main/java/com/yzy/netty/fourthexample/MyServerInitializer.java
@@ -0,0 +1,22 @@
+package com.yzy.netty.fourthexample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author You
+ * @create 2019-06-23 0:54
+ */
+public class MyServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast(new IdleStateHandler(5,7,10,TimeUnit.SECONDS));
+        pipeline.addLast(new MyServerHandle());
+    }
+}
